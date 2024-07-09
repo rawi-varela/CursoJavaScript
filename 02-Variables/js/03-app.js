c28d62b1cab7eb4e72784f10fb32b4a50db37510
“rawi-varela” <rawi.varela@gmail.com>
2024-07-09 14:18:52 -0600

    02-Variables finalizado

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,15 @@
+// Const
+
+// Las variable con const su valor no puede ser re-asignado:
+
+const producto = 'Monitor 30 Pulgadas';
+console.log(producto);
+producto = 'Monitor 23 Pulgadas';
+
+// Por otro lado, las variables con const, deben iniciarse con un valor:
+const precio;
+precio = 20;
+console.log(precio);
+
+// Existen otras diferencias entre const y let especialmente cuando se trabajan con arreglos y objetos, que te ve más adelante.
+
